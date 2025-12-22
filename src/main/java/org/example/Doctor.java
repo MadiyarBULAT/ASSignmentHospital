6dbb45fd1aadeb06444a4ca48c13aac1a1d56b9c
Madiyar <bulatmadiar@gmail.com>
2025-12-22 12:55:34 +0500

    2

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -3,23 +3,55 @@ package org.example;
 import java.util.ArrayList;
 
 public class Doctor {
+
     private String name;
-    private String specilization;
+    private String specialization;
     private ArrayList<Patient> patients;
 
-    public Doctor(String name, String specilization) {
+
+    public Doctor(String name, String specialization) {
         this.name = name;
-        this.specilization = specilization;
+        this.specialization = specialization;
         this.patients = new ArrayList<>();
     }
 
-    public void assignPateint(Patient pateint) {
-        patients.add(pateint);
+
+
+    public String getName() {
+        return name;
     }
 
-    public void showPateints() {
-        System.out.println("Doctor name: " + name + "(" + specilization + ")");
-        for(Patient p : patients) {
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setPatients(ArrayList<Patient> patients) {
+        this.patients = patients;
+    }
+
+
+
+    public void assignPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+    public void showPatients() {
+        System.out.println("Doctor name: " + name + " (" + specialization + ")");
+        for (Patient p : patients) {
             p.showInfo();
         }
     }
