236a8b83a6f232fe0f055c9bf2b7255bdf692ae1
Madiyar <bulatmadiar@gmail.com>
2026-02-02 00:35:46 +0500

    1

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import menu.Menu;
-import menu.HospitalMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        Menu menu = new HospitalMenu();
-        menu.run();
-    }
-}
